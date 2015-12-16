dd6368d69f939ea0e1a0704da0f4c18c7068ca30
armistize <tawit.kalayanamit@gmail.com>
2015-12-16 18:00:46 +0700

    Rename to BuildAction

---
+++ src/main/java/org/jenkinsci/plugins/buildaction/BuildAction.java
@@ -0,0 +1,47 @@
+/**
+ * @author  Tawit K.
+ */
+package org.jenkinsci.plugins.buildaction;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import hudson.model.RootAction;
+
+public class BuildAction implements RootAction {
+
+	private String textBoxValue;
+
+	public BuildAction() {
+		// TODO: initialize
+	}
+
+	public void click() {
+		// TODO: Do your work here after user perform click to your action.
+	}
+
+	public void doConfigSubmit(StaplerRequest request, StaplerResponse response)
+			throws ServletException, IOException, InterruptedException {
+		// TODO: Do your work here after submit button was pressed.
+	}
+
+	public String getDisplayName() {
+		return Constants.DISPLAY_NAME;
+	}
+
+	public String getIconFileName() {
+		return Constants.ICON_PATH;
+	}
+
+	public String getTextBoxValue() {
+		return textBoxValue;
+	}
+
+	public String getUrlName() {
+		return Constants.URL_NAME;
+	}
+}
